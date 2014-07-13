1d647f963ba45c886ae30be2cc263691ee878dd7
tetricas <tetricas@users.noreply.github.com>
2014-07-13 13:28:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <clocale>
 
 using namespace std;
 
 int main()
 {
-    char in;
+    setlocale(LC_ALL, "");
 
-    cout << "Vvedite" << endl;
+    char in[100];
+
+    cout << "Введите" << endl;
     cin >> in;
+
+    for(int i=0; i<=100; i++)
+    {
+        switch (in[i])
+            case 'q':
+                in[i] = 'й';
+                break;
+            case 'w':
+                in[i] = 'ц';
+                break;
+            case 'e':
+                in[i] = 'у';
+                break;
+            case 'r':
+                in[i] = 'к';
+                break;
+            case 't':
+                in[i] = 'е';
+                break;
+            case 'y':
+                in[i] = 'н';
+                break;
+            default:
+                cout << "Ошибка";
+                break;
+    }
+
+    cout << in;
 
     return 0;
 }
